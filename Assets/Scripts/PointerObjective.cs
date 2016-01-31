72b2d120b83aaa251728f1828b7070294141520d
Jordi Santiago <metalzilla@gmail.com>
2016-01-31 15:01:44 +0100

    Fixed menu

--- Assets/Scripts/PointerObjective.cs
+++ Assets/Scripts/PointerObjective.cs
@@ -2,20 +2,30 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 
-public class PointerObjective : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
+public class PointerObjective : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler, ISelectHandler
 {
 	public GameObject Pointer;
 
 	public void OnPointerEnter(PointerEventData eventData)
 	{
-		if (Pointer == null)
-			return;
-
-		Pointer.transform.SetParent(transform, false);
+		DoYourJob();
 	}
 
 	public void OnPointerDown(PointerEventData eventData)
 	{
+		DoYourJob();
+	}
 
+	public void OnSelect(BaseEventData eventData)
+	{
+		DoYourJob();
+	}
+
+	private void DoYourJob()
+	{
+		if (Pointer == null)
+			return;
+
+		Pointer.transform.SetParent(transform, false);
 	}
 }
