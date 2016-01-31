87535e88b91a80c32369751fc3c23f22ecfe9c3a
kondorriano <nicobito@gmail.com>
2016-01-31 14:54:53 +0100

    music menu

---
+++ Assets/Scripts/playMenuMusic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playMenuMusic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        AudioSource a = GetComponent<AudioSource>();
+        a.time = 71f;
+        a.Play();
+	}
+	
+
+}
