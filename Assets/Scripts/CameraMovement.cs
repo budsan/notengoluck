d9111402eba5df0e41873ade684ff405b20c09c6
Jordi Santiago <metalzilla@gmail.com>
2016-01-31 12:58:51 +0100

    Instantiating players

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,19 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraMovement : MonoBehaviour {
-
-	public Transform player;
-
+[RequireComponent(typeof(Camera))]
+public class CameraMovement : MonoBehaviour
+{
 	const float DEEP_FACTOR = 0.6f;
 
 	// Update is called once per frame
-	void LateUpdate () {
-		Vector3 pos = transform.position;
-		pos.x = player.position.x;
-		pos.z = -7f + player.position.z * DEEP_FACTOR;
-		transform.position = pos;
+	void LateUpdate ()
+	{
+		int count = 0;
+		Vector3 med = new Vector3();
+		for (int i = 0; i < Logic.ins.Players.Length; i++)
+		{
+			GameObject player = Logic.ins.Players[i];
+			if (player == null)
+				continue;
 
-		transform.LookAt (player);
+			med += player.transform.position;
+			count++;
+		}
+
+		med /= count;
+		Vector3 pos = transform.position;
+		pos.x = med.x;
+		pos.z = -7f + med.z * DEEP_FACTOR;
+		transform.position = pos;
+		transform.LookAt(med);
+
+		for (int i = 0; i < Logic.ins.Players.Length; i++)
+		{
+			GameObject player = Logic.ins.Players[i];
+			if (player == null)
+				continue;
+		}
 	}
 }
