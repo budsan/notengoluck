6dc699db41f7f4216d97971f014022ba7696b102
kondorriano <nicobito@gmail.com>
2016-01-31 01:02:46 +0100

    All prefabs

---
+++ Assets/Scripts/ChairInstantiation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChairInstantiation : MonoBehaviour {
+
+    public Transform chair;
+
+	// Use this for initialization
+	void Start () {
+        List<Transform> childs = new List<Transform>();
+        foreach(Transform child in transform)
+        {
+            childs.Add(child);
+        }
+
+        int instantiationNum = Random.Range(0, childs.Count + 1);
+        for(int i = 0; i < instantiationNum; ++i)
+        {
+            int index = Random.Range(0, childs.Count);
+            Transform newPos = childs[index];
+            childs.RemoveAt(index);
+
+            Instantiate(chair, newPos.position, newPos.rotation);
+            
+        }
+	}
+
+}
