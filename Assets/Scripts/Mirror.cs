f7e3a56b9324d25dc276eaec01381b23d505d374
Fonserbc <fonserbc@gmail.com>
2016-01-31 03:30:50 +0100

    Working on mirror

---
+++ Assets/Scripts/Mirror.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mirror : MonoBehaviour {
+
+	const float TORQUE = -20000f;
+
+	Rigidbody rb;
+
+	void Start() {
+		rb = GetComponentInChildren<Rigidbody> ();
+	}
+
+	public void Fall() {
+		rb.isKinematic = false;
+		rb.useGravity = true;
+
+		rb.AddRelativeTorque (new Vector3 (TORQUE, 0f, 0f));
+		gameObject.tag = "Unluck";
+	}
+
+	void OnCollisionEnter(Collision col) {
+		if (col.relativeVelocity.magnitude > 1f) {
+			
+		}
+	}
+}
